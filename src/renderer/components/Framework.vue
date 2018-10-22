ba8cac37f5100307cd6c14a9b397aa17ff6ca085
Tomas Buteler <tbuteler@me.com>
2018-10-22 10:29:52 +0100

    Styling improvements

--- src/renderer/components/Framework.vue
+++ src/renderer/components/Framework.vue
@@ -3,7 +3,7 @@
         <div class="header">
             <div class="title">
                 <div class="status">
-                    <span class="indicator"></span>
+                    <span class="indicator" :title="displayStatus(framework.status)"></span>
                 </div>
                 <h3 class="heading">{{ framework.name }}</h3>
                 <div class="float-right">
@@ -16,19 +16,31 @@
                         @click="run"
                     >
                         {{ framework.selective ? 'Run selected' : 'Run' }}
+                        <span v-if="framework.selective" class="Counter">{{ framework.selectedCount.suites }}</span>
                     </button>
                     <button class="btn btn-sm btn-danger" @click="stop" :disabled="!running && !refreshing">Stop</button>
                 </div>
             </div>
-            <div class="progress-bar">
-                <span class="status">
-                    {{ displayStatus(framework.status) }}
-                </span>
-                <div class="float-right">
-                    <TestCount :framework="framework" />
-                </div>
-                <!-- <br>Toggle by status -->
+            <div class="progress">
+                <template v-if="refreshing">Refreshing...</template>
+                <template v-else-if="framework.suites.length">
+                    {{ '1 suite|:n suites' | plural(framework.suites.length) }}
+                    <button
+                        type="button"
+                        class="ellipsis-expander"
+                        :aria-expanded="framework.expandResults"
+                        @click="framework.expandResults = !framework.expandResults"
+                    >&hellip;</button>
+                </template>
+                <template v-else>
+                    No tests loaded. <a href="#" @click.prevent="refresh">Refresh</a>.
+                </template>
             </div>
+            <transition>
+                <div class="progress-breakdown" v-if="framework.expandResults">
+                    <span v-if="framework.selective">{{ '1 selected|:n selected' | plural(framework.selectedCount.suites) }}</span>
+                </div>
+            </transition>
             <transition>
                 <div class="filter-bar" v-if="framework.suites.length > 1">
                     <input class="form-control input-block input-sm" placeholder="Filter tests">
@@ -43,14 +55,12 @@
 import { mapGetters } from 'vuex'
 import Group from '@/components/Group'
 import Suite from '@/components/Suite'
-import TestCount from '@/components/TestCount'
 
 export default {
     name: 'Framework',
     components: {
         Group,
-        Suite,
-        TestCount
+        Suite
     },
     props: {
         framework: {
