15b66b57e8937a88d6ba3cc53c7781e216ada2ad
Tomas Buteler <tbuteler@me.com>
2019-04-21 09:48:57 +0100

    Upgrade to `babel-minify-webpack-plugin`

--- .electron-vue/reporters/webpack.jest.config.js
+++ .electron-vue/reporters/webpack.jest.config.js
@@ -8,7 +8,7 @@ const replacements = getReplacements()
 const path = require('path')
 const webpack = require('webpack')
 
-const BabiliWebpackPlugin = require('babili-webpack-plugin')
+const MinifyPlugin = require('babel-minify-webpack-plugin')
 
 let config = {
     entry: {
@@ -51,7 +51,7 @@ config.plugins.push(
 
 if (process.env.NODE_ENV === 'production') {
     config.plugins.push(
-        new BabiliWebpackPlugin(),
+        new MinifyPlugin(),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': '"production"'
         })
