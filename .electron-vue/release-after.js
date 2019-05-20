da356f32cf0265ea6db4ac85ac4c2ca491e2c05b
Tomas Buteler <tbuteler@me.com>
2019-05-20 21:02:55 +0100

    Merge git tag and git tag push into single line for after release message

--- .electron-vue/release-after.js
+++ .electron-vue/release-after.js
@@ -3,5 +3,4 @@ const chalk = require('chalk')
 
 console.log(`\n${chalk.bgBlue.white(' RELEASE DONE ')}`)
 console.log('Please remember to commit, tag and publish this release:')
-console.log(`git tag v${pkg.version}`)
-console.log(`git push origin refs/tags/v${pkg.version}\n`)
+console.log(`git tag v${pkg.version} && git push origin refs/tags/v${pkg.version}\n`)
