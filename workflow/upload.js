c3ca22b4b4b764fc31036c26ace8446b27e84229
Tomas Buteler <tbuteler@me.com>
2019-09-22 20:39:32 +0100

    Attempt to fix automated build upload on Windows (again)

--- workflow/upload.js
+++ workflow/upload.js
@@ -2,7 +2,6 @@ const _ = require('lodash')
 const Path = require('path')
 const Fs = require('fs-extra')
 const aws = require('aws-sdk')
-const argv = require('yargs').argv
 const s3 = new aws.S3({
     region: 'eu-west-1',
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -43,7 +42,7 @@ for (const file of files) {
         Bucket: process.env.AWS_S3_BUCKET,
         Key: _.compact([
             'dev',
-            argv.prefix,
+            process.env.PATH_PREFIX,
             process.platform,
             file
         ]).join('/')
