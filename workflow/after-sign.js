bb0a03172ef763e30106822080150deb98ee8de1
Tomas Buteler <tbuteler@me.com>
2020-04-23 10:17:00 +0100

    Fix icons not found on build; verify signing and notarization

--- workflow/after-sign.js
+++ workflow/after-sign.js
@@ -1,27 +1,37 @@
 const Path = require('path')
+const { execSync } = require('child_process')
 const builder = require('../electron-builder.json')
 const notarize = require('electron-notarize')
 
 module.exports = async function (params) {
-    // Only notarize the app on macOS and unless it's
-    // been explicitly skipped.
-    if (params.electronPlatformName !== 'darwin' || process.env.NOTARIZE === 'false') {
+    if (params.electronPlatformName !== 'darwin') {
         return
     }
 
-    try {
+    const appPath = Path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`)
+
+    // Verify code signing
+    console.log(`Verifying code signing:`)
+    execSync(`codesign -v -vvv ${appPath}`, { stdio: 'inherit' })
+
+    // Notarize, unless explicitly skipped
+    if (process.env.NOTARIZE !== 'false') {
         console.log(`Starting notarization of ${builder.appId}.`)
 
-        await notarize.notarize({
-            appBundleId: builder.appId,
-            appPath: Path.join(params.appOutDir, `${params.packager.appInfo.productFilename}.app`),
-            appleId: process.env.APPLE_ID,
-            appleIdPassword: process.env.APPLE_ID_PASSWORD
-        })
-    } catch (error) {
-        console.error(error)
-        process.exit(1)
-    }
+        try {
+            await notarize.notarize({
+                appBundleId: builder.appId,
+                appPath,
+                appleId: process.env.APPLE_ID,
+                appleIdPassword: process.env.APPLE_ID_PASSWORD
+            })
+        } catch (error) {
+            console.error(error)
+            process.exit(1)
+        }
 
-    console.log(`Finished notarization of ${builder.appId}.`)
+        console.log(`Finished notarization of ${builder.appId}.`)
+        console.log(`Verifying notarization:`)
+        execSync(`spctl -a -vv ${appPath}`, { stdio: 'inherit' })
+    }
 }
