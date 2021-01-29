45fe9cf76fa2fe9fad520c69b81eb122f5e42179
Tomas Buteler <tbuteler@me.com>
2021-01-29 12:15:20 +0000

    Use custom E2E startup script
    Trying to find a simple workflow that works in CI for both macOS and Windows.

---
+++ workflow/e2e.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const { exec } = require('child_process')
+let callbackId = null
+
+const teardown = () => {
+    if (callbackId) {
+        try {
+            // Try to kill child process, but don't
+            // throw if it no longer exists
+            process.kill(-callbackId)
+        } catch (_) {}
+    }
+    process.exit()
+}
+
+if (process.platform === 'win32') {
+    const rl = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    rl.on('SIGINT', () => {
+        process.emit('SIGINT')
+    })
+}
+
+process.on('SIGINT', () => {
+    teardown()
+})
+
+const startRenderer = require('./runners').startRenderer
+startRenderer().then(() => {
+    const callback = exec(process.argv[2] === 'open'
+        ? 'cypress open'
+        : 'cypress run -b electron'
+    )
+    callbackId = callback.pid
+    callback.stdout.setEncoding('utf8')
+    callback.stderr.setEncoding('utf8')
+    callback.stdout.pipe(process.stdout)
+    callback.stderr.pipe(process.stderr)
+    callback.on('error', teardown)
+    callback.on('close', teardown)
+})
--- workflow/run-renderer.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-
-const startRenderer = require('./runners').startRenderer
-startRenderer()
