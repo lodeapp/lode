45b1d63cb6fc48bac535f18188b7bef64c173f8b
Tomas Buteler <tbuteler@me.com>
2021-04-03 23:03:26 +0100

    More Cypress test refactoring

---
+++ tests/cypress/support/ipc.js
@@ -0,0 +1,31 @@
+import electron from '../../mocks/electron'
+
+Cypress.Commands.add('ipcEvent', (...args) => {
+    // Allow functions that resolve into arguments
+    if (args.length === 1 && typeof args[0] === 'function') {
+        args = args[0]()
+    }
+
+    cy.then(() => {
+        electron.ipcRenderer.trigger(...args)
+    })
+})
+
+Cypress.Commands.add('ipcResetMockHistory', () => {
+    cy.then(() => {
+        if (electron.ipcRenderer.send.resetHistory) {
+            electron.ipcRenderer.send.resetHistory()
+        }
+        if (electron.ipcRenderer.invoke.resetHistory) {
+            electron.ipcRenderer.invoke.resetHistory()
+        }
+    })
+})
+
+Cypress.Commands.add('ipcInvocation', index => {
+    cy.wrap(electron.ipcRenderer.invoke.getCall(index))
+})
+
+Cypress.Commands.add('ipcEmission', index => {
+    cy.wrap(electron.ipcRenderer.send.getCall(index))
+})
---
+++ tests/cypress/support/process.js
@@ -0,0 +1,44 @@
+import { Lode } from '@preload/lode'
+import electron from '../../mocks/electron'
+
+Cypress.Commands.add('start', (options = {}) => {
+    cy
+        .visit('/', {
+            onBeforeLoad (win) {
+                win.Lode = Lode
+            },
+            onLoad (win) {
+                cy.spy(electron.ipcRenderer, 'send')
+                electron.ipcRenderer.trigger('did-finish-load', {
+                    ...{
+                        theme: 'light',
+                        version: '0.0.0',
+                        focus: true
+                    },
+                    ...options
+                })
+            }
+        })
+})
+
+Cypress.Commands.add('startWithProject', (options = {}) => {
+    cy
+        .start(options)
+        .fixture('framework/project.json')
+        .then(project => {
+            electron.ipcRenderer.trigger('project-ready', project)
+        })
+        .wait(1)
+})
+
+Cypress.Commands.add('nextTick', callback => {
+    if (callback) {
+        cy
+            .wait(1)
+            .then(callback)
+            .wait(1)
+    } else {
+        cy
+            .wait(1)
+    }
+})
