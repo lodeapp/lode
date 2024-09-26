0d915102592b4cc5d24528c58b7f569447b22f28
Tomas Buteler <tbuteler@me.com>
2024-09-26 08:39:31 +0100

    Add PHPUnit 10 to packing workflow

--- workflow/reporters/phpunit.js
+++ workflow/reporters/phpunit.js
@@ -3,3 +3,4 @@ const Fs = require('fs-extra')
 
 Fs.mkdirpSync(Path.join(__dirname, '../../static/reporters'))
 Fs.copySync(Path.join(__dirname, '../../src/lib/reporters/phpunit'), Path.join(__dirname, '../../static/reporters/phpunit'))
+Fs.copySync(Path.join(__dirname, '../../src/lib/reporters/phpunit-10'), Path.join(__dirname, '../../static/reporters/phpunit-10'))
